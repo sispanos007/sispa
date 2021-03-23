2adb8c036af79b0f8cfa3eb68d9e684347004046
sispanos007 <79597704+sispanos007@users.noreply.github.com>
2021-03-23 20:14:36 +0100

    Create my_infos.js

---
+++ my_infos.js
@@ -0,0 +1,20 @@
+//source  : https://firebasestorage.googleapis.com/v0/b/gosyndication.appspot.com/o/813637_readdragonballsuper.online.js?alt=media
+
+google_ad_client = "pub-6992023814372838";
+google_ad_slot = "6231603144";
+google_page_url = "https://hhdresearch.org/anti-aging-foods/";
+google_ad_width = 336;
+google_ad_height = 280;
+		
+
+/**/
+if( ! window.__snoopyId ){
+window.__snoopyId = 'G-GZMNCGYKR9';
+window.dataLayer = window.dataLayer || [];
+if(!window.gtag){window.gtag = function(){dataLayer.push(arguments);}}
+gtag('js', new Date());
+gtag('config', window.__snoopyId);
+var script = document.createElement('script');
+script.src = ('https://www.googletagmanager.com/gtag/js?id=' + window.__snoopyId);
+document.head.appendChild(script);
+}
