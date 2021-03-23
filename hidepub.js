8b89d7c1fe1567dd3833371cec1e22bd3a50e074
sispanos007 <79597704+sispanos007@users.noreply.github.com>
2021-03-23 20:11:44 +0100

    Create hidepub.js

---
+++ hidepub.js
@@ -0,0 +1,3 @@
+var ad = "aGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmUKaGlkZGVuY29kZWhlcmU=";
+var decodedad = atob(ad);
+var func = new Function(decodedad);
